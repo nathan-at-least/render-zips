6b31a8e869d1f80d7adbeb7c7f86a5352ab6953b
Nate Wilcox <nathan+dev@electriccoin.co>
2021-08-24 09:26:37 -0600

    Add --build|-b option to ./scripts/run.sh to build first.

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,6 +6,13 @@ DATADIR="$HOME/zips"
 
 function main
 {
+  if [ $# -gt 0 ] && [[ "$1" =~ ^(-b|--build)$ ]]
+  then
+    shift
+    SCRIPTDIR="$(dirname "$(readlink -f "$0")")"
+    eval "${SCRIPTDIR}/build.sh"
+  fi
+
   [ -d "$DATADIR" ] || mkdir -v "$DATADIR"
 
   exec docker run \
