6622e30b48fa625c357a5ed405416d9235815da6
Nate Wilcox <nathan+dev@electriccoin.co>
2021-08-24 09:48:07 -0600

    Tidy up set -x in ./scripts/

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -x
 set -efuo pipefail
 
 # Get the project dir regardless of pwd or symlinks:
 BASEDIR="$(dirname "$(dirname "$(readlink -f "$0")")")"
 
+set -x
 cd "$BASEDIR"
 exec docker build -t render-zips .
